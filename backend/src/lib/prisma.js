e037b9fd70d539fc3bb0567bf4577f7cfc000083
Muhammed Shafeeh <98475872+shafeehhecker@users.noreply.github.com>
2026-05-26 08:54:41 +0300

    Create prisma.js

---
+++ backend/src/lib/prisma.js
@@ -0,0 +1,9 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error'],
+});
+
+process.on('beforeExit', async () => { await prisma.$disconnect(); });
+
+module.exports = prisma;
